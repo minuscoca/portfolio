ec2063242252ba7982a77a4dee0308a4fe9e6129
Kevin <minuscoca@gmail.com>
2023-09-22 17:55:11 +0800

    prettier

--- src/app/_sections/projects-section/app-store-button.tsx
+++ src/app/_sections/projects-section/app-store-button.tsx
@@ -5,7 +5,11 @@ import { Button } from "@/components/ui/button";
 export function AppStoreButton({ href }: { href?: string }) {
   if (!href) return null;
   return (
-    <Button variant="link" onClick={() => window.open(href, "_blank")}>
+    <Button
+      variant="link"
+      onClick={() => window.open(href, "_blank")}
+      className="font-thin text-sky-600 dark:text-sky-400"
+    >
       前往 App Store
     </Button>
   );
--- src/app/_sections/projects-section/projects.tsx
+++ src/app/_sections/projects-section/projects.tsx
@@ -2,12 +2,15 @@ import { type Project } from "@/_data/projects";
 import { type Stack } from "@/_data/stacks";
 import { Badge } from "@/components/ui/badge";
 import { AppStoreButton } from "./app-store-button";
-import Image from 'next/image'
+import Image from "next/image";
 
 export function Project({ project }: { project: Project }) {
   return (
-    <div className="grid w-full grid-cols-1 sm:grid-cols-2 rounded-xl overflow-hidden group cursor-pointer bg-rose-100 dark:bg-rose-800/60">
-      <div id="project_detail" className="relative min-h-[10rem] flex flex-col px-4 group-even:order-last">
+    <div className="group grid w-full cursor-pointer grid-cols-1 overflow-hidden rounded-xl bg-rose-100 dark:bg-rose-800/50 sm:grid-cols-2">
+      <div
+        id="project_detail"
+        className="relative flex min-h-[10rem] flex-col px-4 group-even:order-last"
+      >
         <ProjectHeader project={project} />
         <ProjectDesc desc={project.desc} />
         <ProjectStacks stacks={project.stacks} />
@@ -21,11 +24,11 @@ export function Project({ project }: { project: Project }) {
 
 function ProjectStacks({ stacks }: { stacks: Stack[] }) {
   return (
-    <ul className="flex justify-center w-full p-4 flex-wrap">
+    <ul className="flex w-full flex-wrap justify-center p-4">
       {stacks.map((stack) => (
         <Badge
           key={stack.key}
-          className="cursor-pointer rounded-full active:opacity-25 min-w-[5rem] mx-2 my-1"
+          className="mx-2 my-1 min-w-[5rem] cursor-pointer rounded-full bg-slate-700 px-4 py-1 active:opacity-25 dark:bg-slate-100"
         >
           <span className="w-full">{stack.name}</span>
         </Badge>
@@ -37,44 +40,51 @@ function ProjectStacks({ stacks }: { stacks: Stack[] }) {
 function ProjectHeader({ project }: { project: Project }) {
   return (
     <>
-      <h1 className="text-3xl font-extrabold px-4 mt-4">{project.name}</h1>
+      <h1 className="mt-4 px-4 text-3xl font-extrabold">{project.name}</h1>
       <AppStoreButton href={project.links?.appStore} />
-      <span className="w-full h-[1px] rounded bg-foreground/30 mt-2" />
+      <span className="mt-2 h-[1px] w-full rounded bg-foreground/30" />
     </>
-  )
+  );
 }
 
 function ProjectDesc({ desc }: { desc: string }) {
   return (
-    <p className="flex-1 whitespace-pre-line text-start text-sm leading-6 p-4">{desc}</p>
-  )
+    <p className="flex-1 whitespace-pre-line p-4 text-start text-sm leading-6">
+      {desc}
+    </p>
+  );
 }
 
 function ProjectImage({ project }: { project: Project }) {
   return (
-    <div id="project_image" className="
-      absolute w-full h-full rounded-xl overflow-hidden transition-all rotate
-      right-4
-      group-even:-right-4
-      origin-top-left
-      group-even:origin-top-right
+    <div
+      id="project_image"
+      className="
+      rotate absolute right-4 h-full w-full origin-top-left translate-x-4
       translate-y-4
-      translate-x-4
+      overflow-hidden
+      rounded-xl
+      transition-all
+      group-even:-right-4
+      group-even:origin-top-right
       group-even:-translate-x-4
-      group-hover:-rotate-1
-      group-even:group-hover:rotate-1
       group-hover:translate-y-16
+      group-hover:-rotate-1
       group-hover:scale-110
-    ">
-      {project.images
-        ? <Image
+      group-even:group-hover:rotate-1
+    "
+    >
+      {project.images ? (
+        <Image
           alt={`${project.key}_sample_image`}
           src={project.images[0]}
           fill
-          objectFit='cover'
-          className='object-contain object-left'
+          objectFit="cover"
+          className="object-contain object-left"
         />
-        : <div>Empty Image</div>}
+      ) : (
+        <div>Empty Image</div>
+      )}
     </div>
-  )
+  );
 }
