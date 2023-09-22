d22122b12dbae7911650a5c44b095634d030b38b
Kevin <minuscoca@gmail.com>
2023-09-22 15:01:56 +0800

    update project

--- src/_data/projects.ts
+++ src/_data/projects.ts
@@ -2,7 +2,7 @@ import { Stacks, type Stack } from "./stacks";
 
 type K =
   | "merchant_app"
-  | "driver_app"
+  | "dimrider"
   | "customer_app"
   | "admin_pannel"
   | "merchant_dashboard";
@@ -10,34 +10,52 @@ type K =
 export type Project = {
   key: keyof typeof Projects;
   name: string;
+  desc: string;
   stacks: Stack[];
-  images?: [];
+  images?: string[];
 };
 
 export const Projects: Record<K, Project> = {
   merchant_app: {
     key: "merchant_app",
     name: "POS",
+    desc: "",
     stacks: [Stacks["react-native"], Stacks.redux],
   },
-  driver_app: {
-    key: "driver_app",
-    name: "Driver App",
+  dimrider: {
+    key: "dimrider",
+    name: "DimRider",
+    desc: "提供DimOrder點單司機承接DimOrder外送訂單，查看外送訂單內容的便捷工具。\n\nDimRider 為專為DimOrder點單司機設計的接單App。透過DimRider，可輕鬆查看目前的外送訂單狀況，並可一鍵承接訂單。於訂定畫面中，可一目暸然目前外送訂單的取餐時間、預計送達時間。訂單數量，也方便與DimOrder營運人員聯繫。在設定畫面中，也可查詢個人的接單數據統計，方便查詢自己的歷史接單紀錄。",
     stacks: [Stacks["react-native"], Stacks.redux],
+    images: [
+      '/project-images/dimrider/img-0.png',
+      '/project-images/dimrider/img-1.jpg',
+      '/project-images/dimrider/img-2.jpg',
+      '/project-images/dimrider/img-3.jpg',
+      '/project-images/dimrider/img-4.jpg',
+      '/project-images/dimrider/img-5.jpg',
+      '/project-images/dimrider/img-6.jpg',
+      '/project-images/dimrider/img-7.jpg',
+      '/project-images/dimrider/img-8.jpg',
+      '/project-images/dimrider/img-9.jpg',
+    ]
   },
   customer_app: {
     key: "customer_app",
     name: "Customer App",
+    desc: "",
     stacks: [Stacks.react, Stacks.redux, Stacks["@tanstack/react-query"]],
   },
   admin_pannel: {
     key: "admin_pannel",
     name: "Admin Web",
+    desc: "",
     stacks: [Stacks.react, Stacks.redux, Stacks["@tanstack/react-table"]],
   },
   merchant_dashboard: {
     key: "merchant_dashboard",
     name: "Dashboard",
+    desc: "",
     stacks: [
       Stacks.react,
       Stacks.nextjs,
--- src/app/_sections/projects-section/projects.tsx
+++ src/app/_sections/projects-section/projects.tsx
@@ -1,32 +1,65 @@
 import { type Project } from "@/_data/projects";
 import { type Stack } from "@/_data/stacks";
 import { Badge } from "@/components/ui/badge";
+import Image from 'next/image'
 
 export function Project({ project }: { project: Project }) {
   return (
-    <div className="grid w-full grid-cols-1 sm:grid-cols-2">
-      <div id="project_detail" className="aspect-square bg-rose-500">
-        <h1>{project.name}</h1>
+    <div className="grid w-full grid-cols-1 sm:grid-cols-2 rounded-xl overflow-hidden group cursor-pointer">
+      <div id="project_detail" className="min-h-[10rem] flex flex-col bg-rose-100 dark:bg-rose-800/75 px-4">
+        <ProjectName name={project.name} />
+        <ProjectDesc desc={project.desc} />
         <ProjectStacks stacks={project.stacks} />
       </div>
-      <div id="project_image" className="aspect-square bg-secondary">
-        <div>Images</div>
-      </div>
+      <ProjectImage project={project} />
     </div>
   );
 }
 
 function ProjectStacks({ stacks }: { stacks: Stack[] }) {
   return (
-    <ul className="grid grid-cols-3 gap-4">
+    <ul className="flex justify-center w-full p-4 flex-wrap">
       {stacks.map((stack) => (
         <Badge
           key={stack.key}
-          className="cursor-pointer rounded-full active:opacity-25"
+          className="cursor-pointer rounded-full active:opacity-25 min-w-[5rem] mx-2 my-1"
         >
           <span className="w-full">{stack.name}</span>
         </Badge>
       ))}
     </ul>
   );
+}
+
+function ProjectName({ name }: { name: string }) {
+  return (
+    <>
+      <h1 className="text-3xl font-extrabold p-4">{name}</h1>
+      <span className="w-full h-[1px] rounded bg-foreground/30" />
+    </>
+  )
+}
+
+function ProjectDesc({ desc }: { desc: string }) {
+  return (
+    <>
+      <p className="flex-1 whitespace-pre-line text-start text-sm leading-6 p-4">{desc}</p>
+    </>
+  )
+}
+
+function ProjectImage({ project }: { project: Project }) {
+  return (
+    <div id="project_image" className="relative bg-secondary aspect-square">
+      <div className="relative w-full h-full overflow-hidden transition-all group-hover:scale-110">
+        {project.images
+          ? <Image
+            alt={`${project.key}_image`}
+            src={project.images[0]}
+            fill
+          />
+          : <div>Empty Image</div>}
+      </div>
+    </div>
+  )
 }
