a633c9e46388db3b4381da7b5f91a0edc7c5c220
Kevin <minuscoca@gmail.com>
2023-09-24 17:04:04 +0800

    update intro

--- src/app/_sections/intro-section/index.tsx
+++ src/app/_sections/intro-section/index.tsx
@@ -9,13 +9,12 @@ export function IntroSection() {
     <Section id="home">
       <PortraitAvatar />
       <motion.h1
-        className="text-lg"
+        className="text-lg whitespace-pre-line"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <b>Hi，我是王楷筌</b>，也可以叫我 Kevin。 我是一名<b>前端工程師</b>
-        ，擅長使用 <i>React</i>、<i>React Native</i> 和 <i>Next.js</i>{" "}
-        這些技術來開發<b>網頁和手機應用程式</b>。
+        <b>Hi，我是王楷筌</b>，也可以叫我 Kevin。 我是一名<b>前端工程師</b>。<br />
+        我擅長使用 <i>React</i>、<i>React Native</i> 和 <i>Next.js</i> 這些技術來開發 <b>網頁和手機應用程式</b>。
       </motion.h1>
 
       <motion.h1
@@ -24,8 +23,7 @@ export function IntroSection() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.25 }}
       >
-        我熱愛編程，並且致力於不斷學習和提升自己的技術能力，以創造出更好的用戶體驗和功能豐富的應用程式。
-        我期待能夠與您一同合作，共同打造出優秀的網頁或應用程式。
+
       </motion.h1>
       <SectionDivider />
     </Section>
