5bf064c34443436909e91421a17a3159200d4819
Kevin <minuscoca@gmail.com>
2023-09-24 16:15:12 +0800

    remove unused link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-import { ThemeToggle } from "@/components/theme/theme-toggle";
 import Link from "next/link";
 
 export function Navbar() {
@@ -19,9 +18,6 @@ export function Navbar() {
         </Link>
         <Link className="px-2 py-1" replace href="/#experience">
           Experience
-        </Link>
-        <Link className="px-2 py-1" replace href="/#contact">
-          Contact
         </Link>
       </nav>
     </header>
